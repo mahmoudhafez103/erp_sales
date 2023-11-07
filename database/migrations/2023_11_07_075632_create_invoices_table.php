131938c48c9c1ad073d305d7593388ffaba67c7c
mahmoudhafez103 <hafz7062@gmail.com>
2023-11-07 16:18:12 +0200

    add migrations and routes

---
+++ database/migrations/2023_11_07_075632_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('invoice_date');
+            $table->bigInteger('customer_code')->nullable();
+            $table->boolean('is_approved')->default(0);
+            $table->text('notes')->default('NULL')->nullable();
+            $table->decimal('discount_value', 10, 2)->default(0.00);
+            $table->decimal('total_cost_items', 10, 2)->default(0.00);
+            $table->decimal('total_befor_discount', 10, 2)->default(0.00);
+            $table->decimal('total_cost', 10, 2)->default(0.00)->nullable();
+            $table->enum('pill_type', ['now', 'later'])->default('NULL')->nullable();
+            $table->integer('added_by');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
---
+++ database/migrations/2023_11_07_075726_create_invoice_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('invoice_id');
+            $table->integer('store_id');
+            $table->integer('item_code');
+            $table->decimal('quantity', 10, 2);
+            $table->decimal('unit_price', 10, 2);
+            $table->decimal('total_price', 10, 2);
+            $table->integer('added_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_details');
+    }
+};
