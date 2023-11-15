99522bad87318601797388ee3fe6b0f8cf8638e9
Krm <https://github.com/KaramRamdan/flutter_1>
2023-11-16 01:43:03 +0200

    frist

---
+++ resources/views/layout/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layout.partials.head')
+<body>
+    @include('layout.partials.header')
+    <div class="container">
+        @yield('content')
+      </div>
+      
+    
+    
+</body>
+
+@include('layout.partials.footer')
+
+</html>
+
+
+
+
